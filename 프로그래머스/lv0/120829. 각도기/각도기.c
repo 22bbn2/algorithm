e24d884dc42b145819d0ba735a1ddc06878c38f2
채연 <126773438+22bbn2@users.noreply.github.com>
2023-03-02 20:12:50 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.c
+++ 프로그래머스/lv0/120829. 각도기/각도기.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 
 int solution(int angle) {
+    
     if(0 < angle && angle < 90)
     {
         return 1;
     }
-    else if(angle == 90)
+    if(angle == 90)
     {
-         return 2;
+        return 2;
     }
-    else if(90 < angle  && angle < 180)
+    if(90 < angle && angle < 180)
     {
-         return 3;
+        return 3;
     }
-    else
+    if(angle == 180)
     {
         return 4;
     }
-   
 }
