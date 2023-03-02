df8fda8dcc06aaa538f8df506d28fbbdd59ed6b8
채연 <126773438+22bbn2@users.noreply.github.com>
2023-03-02 20:44:50 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,13 @@
+function solution(slice, n) {
+    var answer = 0;
+    for(var i = 0 ; i <= n ; i++)
+    {
+        if((slice * i) >= n)
+        {
+            return i;
+        }
+        
+        continue;
+    }
+    //return answer;
+}
