fa5bea65817786d90aec58ef67c6f2aca98e4286
채연 <126773438+22bbn2@users.noreply.github.com>
2023-03-09 00:05:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int ans;
+
+        if(a == b & b == c){
+            ans = 10000+a*1000;
+        }
+        else if(a == b || a == c){
+            ans = 1000+a*100;
+        }
+        else if(b == c){
+            ans = 1000+b*100;
+        }
+        else{
+            ans = (Math.max(Math.max(a, b), c)) * 100;
+        }
+
+        System.out.println(ans);
+    }
+}
