d831d3f45af78a22dcb2ea4d25cc3a4e57a89549
채연 <126773438+22bbn2@users.noreply.github.com>
2024-02-12 17:16:53 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 524 ms, Memory: 63664 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,36 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+const fs = require('fs')
+
+const getInput = (filePath) => {
+    return fs
+        .readFileSync(filePath)
+        .toString()
+        .trim()
+        .split(/\n/);
+    }
+
+const input = getInput('/dev/stdin'); // 제출시 여기 '/dev/stdin' 으로 변경
+
+let count = Number(input[0]);
+let data = new Array();
+
+for(let i = 1; i <= count ; i ++)
+{
+    let [x,y] = input[i].split(" ").map(Number);
+    data.push([x,y]);
+}
+
+function compare1(a,b)
+{
+    if(a[1] != b[1]) return a[1] - b[1];
+    else return a[0] - b[0];
+}
+
+data.sort(compare1);
+
+let answer = "";
+for(let p of data)
+{
+    answer += p[0] + " " + p[1] + "\n";
+}
+console.log(answer);
